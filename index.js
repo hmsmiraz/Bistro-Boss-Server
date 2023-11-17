92ae9b1aae24051ac9001867784a66f27eb7dd53
hmsmiraz <hmsmiraz64729@gmail.com>
2023-11-18 00:07:43 +0600

    added cart deleted

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.meftkqt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,15 +42,24 @@ async function run() {
 
     // cart's api
     app.get('/carts', async(req, res)=>{
-      const result = await cartCOllection.find().toArray();
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCOllection.find(query).toArray();
       res.send(result);
     });
 
-    app.post("/carts", async (req, res) => {
+    app.post('/carts', async (req, res) => {
       const cartItem = req.body;
       const result = await cartCOllection.insertOne(cartItem);
       res.send(result);
     });
+
+    app.delete('/carts/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCOllection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
