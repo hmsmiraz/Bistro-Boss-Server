f4479563f050bb1d15dc6e9b5789a5fdae46e98c
hmsmiraz <hmsmiraz64729@gmail.com>
2023-11-21 14:33:10 +0600

    try to add payment system

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -190,6 +192,20 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await cartCOllection.deleteOne(query);
       res.send(result);
+    });
+
+    // payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+       const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+       });
+       res.send({
+        clientSecret: paymentIntent.client_secret
+       })
     });
 
     // Send a ping to confirm a successful connection
