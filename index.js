5d9dd76cbbb01f4e8d4077b71c5e596ed137e302
hmsmiraz <hmsmiraz64729@gmail.com>
2023-11-23 03:39:31 +0600

    added admin stats

--- index.js
+++ index.js
@@ -233,6 +233,34 @@ async function run() {
       res.send({paymentResult, deleteResult});
     })
 
+    app.get('/admin-stats', verifyToken, verifyAdmin, async(req, res)=>{
+      const users = await userCOllection.estimatedDocumentCount();
+      const menuItems = await menuCOllection.estimatedDocumentCount();
+      const orders = await paymentCOllection.estimatedDocumentCount();
+
+      // this is not the best way
+      // const payments = await paymentCollection.find().toArray();
+      // const revenue = payments.reduce((total, payment) => total + payment.price, 0);
+      const result = await paymentCOllection.aggregate([
+        {
+          $group: {
+            _id: null,
+            totalRevenue: {
+              $sum: '$price'
+            }
+          }
+        }
+      ]).toArray();
+
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+      res.send({
+        users,
+        menuItems,
+        orders,
+        revenue
+      })
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
